b331fa2128e173c9bdd1e04b7c1dcd8bcd2704d2
walterXXXXX <icsystems@mail.ru>
2024-07-31 14:43:15 +0300

    Add Lesson 06

---
+++ Lesson 06/Task1/mainwindow.cpp
@@ -0,0 +1,114 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->sb_initNum->setValue(200000);
+    ui->rb_synchOff->setChecked(true);
+
+   race1 = new Controller(&m);
+   race2 = new Controller(&m);
+
+   concurRace1 = new ExampleRace(&m);
+   concurRace2 = new ExampleRace(&m);
+
+    //Сигналы по завершению работы потоков
+    connect(race1, &Controller::sig_WorkFinish, [&](){
+        //отображаем только когда второй поток закончит работу
+        if(countFinish == 0){
+            countFinish++;
+        }
+        else{
+            ui->te_debug->append("Искомое число равно: " + QString::number(number) + ", а должно быть " +
+                                  QString::number(ui->sb_initNum->value()*2));
+            ui->pb_start->setEnabled(true);
+        }
+
+    });
+
+    connect(race2, &Controller::sig_WorkFinish, [&](){
+
+        if(countFinish == 0){
+            countFinish++;
+        }
+        else{
+            ui->te_debug->append("Искомое число равно: " + QString::number(number) + ", а должно быть " +
+                                 QString::number(ui->sb_initNum->value()*2));
+            ui->pb_start->setEnabled(true);
+        }
+
+    });
+
+
+    connect(concurRace1, &ExampleRace::sig_Finish, [&](){
+
+        if(countFinish == 0){
+            countFinish++;
+        }
+        else{
+            ui->te_debug->append("Искомое число равно: " + QString::number(number) + ", а должно быть " +
+                                 QString::number(ui->sb_initNum->value()*2));
+            ui->pb_start->setEnabled(true);
+        }
+    });
+
+    connect(concurRace2, &ExampleRace::sig_Finish, [&](){
+
+        if(countFinish == 0){
+            countFinish++;
+        }
+        else{
+            ui->te_debug->append("Искомое число равно: " + QString::number(number) + ", а должно быть " +
+                                 QString::number(ui->sb_initNum->value()*2));
+            ui->pb_start->setEnabled(true);
+        }
+    });
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//Метод запускает два потока
+void MainWindow::StartRace(void){
+
+
+    if(ui->rb_qtConcur->isChecked()){
+
+        // вариант с цепочкой вычислений then()
+
+        auto lmbRace1 = [this]{concurRace1->DoWork(&number, false, ui->sb_initNum->value());};
+        auto lmbRace2 = [this]{concurRace2->DoWork(&number, false, ui->sb_initNum->value());};
+        QtConcurrent::run(lmbRace1).then(lmbRace2);
+
+        // вариант с использованием QFutureWatcher
+
+//        QFutureWatcher<void> *futRace1Wh = new QFutureWatcher<void>(this);
+//        QFuture<void> futRace1 = QtConcurrent::run(&ExampleRace::DoWork, concurRace1, &number, false, ui->sb_initNum->value());
+//        futRace1Wh->setFuture(futRace1);
+//        connect(futRace1Wh, &QFutureWatcher<void>::finished, this, [this]{
+//            QtConcurrent::run(&ExampleRace::DoWork, concurRace2, &number, false, ui->sb_initNum->value());
+//        });
+
+    }
+    else{
+        race1->operate(&number, ui->rb_mutexOn->isChecked(), ui->sb_initNum->value());
+        race2->operate(&number, ui->rb_mutexOn->isChecked(), ui->sb_initNum->value());
+    }
+}
+
+//Обработка кнопки "Старт"
+void MainWindow::on_pb_start_clicked()
+{
+    ui->pb_start->setEnabled(false);
+    countFinish = 0;
+    number = 0;
+    StartRace( );
+}
+
